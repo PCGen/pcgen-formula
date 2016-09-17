c4b1aee7d1a7240095ecefdc1e1f46121238a016
Eitan Adler <lists@eitanadler.com>
2016-09-17 10:15:39 -0700

    convert to local

--- PCGen-Formula/code/src/test/pcgen/base/formula/inst/SimpleFormulaManagerTest.java
+++ PCGen-Formula/code/src/test/pcgen/base/formula/inst/SimpleFormulaManagerTest.java
@@ -27,7 +27,6 @@ import pcgen.base.solver.SolverFactory;
 public class SimpleFormulaManagerTest extends TestCase
 {
 
-	private LegalScopeLibrary scopeLibrary;
 	private VariableLibrary varLibrary;
 	private SimpleFunctionLibrary ftnLibrary;
 	private SimpleOperatorLibrary opLibrary;
@@ -38,7 +37,7 @@ public class SimpleFormulaManagerTest extends TestCase
 	protected void setUp() throws Exception
 	{
 		super.setUp();
-		scopeLibrary = new LegalScopeLibrary();
+		LegalScopeLibrary scopeLibrary = new LegalScopeLibrary();
 		varLibrary = new VariableLibrary(scopeLibrary);
 		opLibrary = new SimpleOperatorLibrary();
 		ftnLibrary = new SimpleFunctionLibrary();
