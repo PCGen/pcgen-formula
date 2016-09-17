a05c6b6fd820087fb5ef9ef51f5c8c60a33284bc
Eitan Adler <lists@eitanadler.com>
2016-09-17 10:35:11 -0700

    crlf -> lf

--- PCGen-Formula/code/src/test/pcgen/base/formula/function/IfFunctionTest.java
+++ PCGen-Formula/code/src/test/pcgen/base/formula/function/IfFunctionTest.java
@@ -20,9 +20,6 @@ package pcgen.base.formula.function;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import org.junit.Test;
 
 import pcgen.base.formatmanager.FormatUtilities;
 import pcgen.base.formula.base.OperatorLibrary;
@@ -36,7 +33,8 @@ import pcgen.base.formula.parse.SimpleNode;
 import pcgen.base.formula.visitor.ReconstructionVisitor;
 import pcgen.base.testsupport.AbstractFormulaTestCase;
 import pcgen.base.testsupport.TestUtilities;
-import pcgen.base.util.FormatManager;
+
+import org.junit.Test;
 
 public class IfFunctionTest extends AbstractFormulaTestCase
 {
